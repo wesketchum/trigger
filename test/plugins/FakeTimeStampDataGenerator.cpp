59abdd7417998541b7dd4bfb9117438e8b3c012b
Thiago Bezerra <thiagojcb@gmail.com>
2021-04-14 06:12:15 -0500

    Adding v0 of FakeTimeStampDataGenerator module.

---
+++ test/plugins/FakeTimeStampDataGenerator.cpp
@@ -0,0 +1,197 @@
+/**
+ * @file FakeTimeStampDataGenerator.cpp FakeTimeStampDataGenerator class
+ * implementation
+ *
+ * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "CommonIssues.hpp"
+#include "FakeTimeStampDataGenerator.hpp"
+
+#include "appfwk/app/Nljs.hpp"
+
+#include "logging/Logging.hpp"
+
+#include <chrono>
+#include <cstdlib>
+#include <thread>
+#include <string>
+#include <vector>
+
+using pd_clock = std::chrono::duration<double, std::ratio<1, 50000000>>;
+
+using namespace triggeralgs;
+
+namespace dunedaq {
+namespace listrev {
+
+FakeTimeStampDataGenerator::FakeTimeStampDataGenerator(const std::string& name)
+  : dunedaq::appfwk::DAQModule(name)
+  , thread_(std::bind(&FakeTimeStampDataGenerator::do_work, this, std::placeholders::_1))
+  , outputQueues_()
+  , queueTimeout_(100)
+  , generator()
+{
+  register_command("conf",  &FakeTimeStampDataGenerator::do_configure);
+  register_command("start", &FakeTimeStampDataGenerator::do_start);
+  register_command("stop",  &FakeTimeStampDataGenerator::do_stop);
+  register_command("scrap", &FakeTimeStampDataGenerator::do_scrap);
+}
+
+void
+FakeTimeStampDataGenerator::init(const nlohmann::json& init_data)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering init() method";
+  auto ini = init_data.get<appfwk::app::ModInit>();
+  for (const auto& qi : ini.qinfos) {
+    if (qi.dir != "output") {
+      continue;                 // skip all but "output" direction
+    }
+    try
+    {
+      //outputQueues_.emplace_back(new sink_t(qi.inst));
+      outputQueue_.reset(new sink_t(qi.inst));
+    }
+    catch (const ers::Issue& excpt)
+    {
+      throw InvalidQueueFatalError(ERS_HERE, get_name(), qi.name, excpt);
+    }
+  }
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Exiting init() method";
+}
+
+void
+FakeTimeStampDataGenerator::get_info(opmonlib::InfoCollector& /*ci*/, int /*level*/)
+{
+}
+
+void
+FakeTimeStampDataGenerator::do_configure(const nlohmann::json& obj)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering do_configure() method";
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Exiting do_configure() method";
+}
+
+void
+FakeTimeStampDataGenerator::do_start(const nlohmann::json& /*args*/)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering do_start() method";
+  thread_.start_working_thread();
+  TLOG() << get_name() << " successfully started";
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Exiting do_start() method";
+}
+
+void
+FakeTimeStampDataGenerator::do_stop(const nlohmann::json& /*args*/)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering do_stop() method";
+  thread_.stop_working_thread();
+  TLOG() << get_name() << " successfully stopped";
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Exiting do_stop() method";
+}
+
+void
+FakeTimeStampDataGenerator::do_scrap(const nlohmann::json& /*args*/)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering do_scrap() method";
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Exiting do_scrap() method";
+}
+
+std::vector<TimeStampedData>
+FakeTimeStampDataGenerator::GetTimeStamp()
+{
+  std::vector<TimeStampedData> tsds;
+  TimeStampedData tsd{};
+
+  int signaltype = rdm_signaltype(generator);
+
+  auto tsd_start_time = std::chrono::steady_clock::now();
+  tsd.time_stamp = (uint64_t)pd_clock(tsd_start_time.time_since_epoch()).count();
+  //std::cout << "\033[32mtsd.timestamp: " << tsd.time_stamp << "\033[0m  ";
+  tsd.signal_type = signaltype;
+  std::cout << "\033[32m" << tsd.time_stamp << ","<< tsd.signal_type << ","<< tsd.counter << "\033[0m\n";
+  auto now = std::chrono::steady_clock::now();
+  tsd.counter = (uint32_t)pd_clock(now.time_since_epoch()).count();
+  tsds.push_back(tsd);
+  
+  return tsds;
+}
+
+void
+FakeTimeStampDataGenerator::do_work(std::atomic<bool>& running_flag)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering do_work() method";
+  size_t generatedCount = 0;
+  size_t sentCount = 0;
+
+  while (running_flag.load()) 
+  {
+    TLOG_DEBUG(TLVL_GENERATION) << get_name() << ": Start of sleep between sends";
+    std::this_thread::sleep_for(std::chrono::nanoseconds(1000000000));
+
+    std::vector<TimeStampedData> tsds = GetTimestamp();
+
+    if (tsds.size() == 0) 
+    {
+      std::ostringstream oss_prog;
+      oss_prog << "Last TSDs packet has size 0, continuing!";
+      ers::debug(ProgressUpdate(ERS_HERE, get_name(), oss_prog.str()));
+      continue; 
+    } 
+    else 
+    {
+      std::ostringstream oss_prog;
+      ers::debug(ProgressUpdate(ERS_HERE, get_name(), oss_prog.str()));
+    }
+
+    generatedCount+=tsds.size();
+
+    for (auto& outQueue : outputQueues_)
+    {
+      std::string thisQueueName = outQueue->get_name();
+      bool successfullyWasSent = false;
+      while (!successfullyWasSent && running_flag.load())
+      {
+        TLOG_DEBUG(TLVL_GENERATION) << get_name() << ": Pushing the generated TSD onto queue " << thisQueueName;
+	for (auto const& tsd: tsds)
+	{
+	  try
+	  {
+	    outQueue->push(tsd, queueTimeout_);
+	    successfullyWasSent = true;
+	    ++sentCount;
+	  }
+	  catch (const dunedaq::appfwk::QueueTimeoutExpired& excpt)
+	  {
+	    std::ostringstream oss_warn;
+	    oss_warn << "push to output queue \"" << thisQueueName << "\"";
+	    ers::warning(dunedaq::appfwk::QueueTimeoutExpired(ERS_HERE, get_name(), oss_warn.str(),
+							      std::chrono::duration_cast<std::chrono::milliseconds>(queueTimeout_).count()));
+	  }
+	}
+      }
+    }
+    if (outputQueues_.size() == 0)
+    {
+      ers::warning(NoOutputQueuesAvailableWarning(ERS_HERE, get_name()));
+    }
+
+  }
+
+  std::ostringstream oss_summ;
+  oss_summ << ": Exiting the do_work() method, generated " << generatedCount
+           << " TSD set and successfully sent " << sentCount << " copies. ";
+  ers::info(ProgressUpdate(ERS_HERE, get_name(), oss_summ.str()));
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Exiting do_work() method";
+}
+
+} // namespace trigger 
+} // namespace dunedaq
+
+DEFINE_DUNE_DAQ_MODULE(dunedaq::trigger::FakeTimeStampDataGenerator)
+
+// Local Variables:
+// c-basic-offset: 2
+// End:
---
+++ test/plugins/FakeTimeStampDataGenerator.hpp
@@ -0,0 +1,95 @@
+/**
+ * @file RandomDataListGenerator.hpp
+ *
+ * RandomDataListGenerator is a simple DAQModule implementation that
+ * periodically generates a list of random integers.
+ *
+ * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef TRIGGER_TEST_PLUGINS_FAKETIMESTAMPDATAGENERATOR_HPP_
+#define TRIGGER_TEST_PLUGINS_FAKETIMESTAMPDATAGENERATOR_HPP_
+
+#include "dune-trigger-algs/TimeStampedData.hh"
+
+#include "appfwk/DAQModule.hpp"
+#include "appfwk/DAQSink.hpp"
+#include "appfwk/ThreadHelper.hpp"
+
+#include <ers/Issue.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <random>
+
+using namespace triggeralgs;
+
+namespace dunedaq {
+namespace trigger {
+
+/**
+ * @brief RandomDataListGenerator creates vectors of ints and writes
+ * them to the configured output queues.
+ */
+class FakeTimeStampDataGenerator : public dunedaq::appfwk::DAQModule
+{
+public:
+  /**
+   * @brief FakeTimeStampDataGenerator Constructor
+   * @param name Instance name for this FakeTimeStampDataGenerator instance
+   */
+  explicit FakeTimeStampDataGenerator(const std::string& name);
+
+  FakeTimeStampDataGenerator(const FakeTimeStampDataGenerator&) =
+    delete; ///< FakeTimeStampDataGenerator is not copy-constructible
+  FakeTimeStampDataGenerator& operator=(const FakeTimeStampDataGenerator&) =
+    delete; ///< FakeTimeStampDataGenerator is not copy-assignable
+  FakeTimeStampDataGenerator(FakeTimeStampDataGenerator&&) =
+    delete; ///< FakeTimeStampDataGenerator is not move-constructible
+  FakeTimeStampDataGenerator& operator=(FakeTimeStampDataGenerator&&) =
+    delete; ///< FakeTimeStampDataGenerator is not move-assignable
+
+  void init(const nlohmann::json& obj) override;
+  void get_info(opmonlib::InfoCollector& ci, int level) override;
+
+private:
+  // Commands
+  void do_configure(const nlohmann::json& obj);
+  void do_start(const nlohmann::json& obj);
+  void do_stop(const nlohmann::json& obj);
+  void do_unconfigure(const nlohmann::json& obj);
+
+  // Threading
+  dunedaq::appfwk::ThreadHelper thread_;
+  void do_work(std::atomic<bool>&);
+
+  // Configuration
+  using sink_t = dunedaq::appfwk::DAQSink<TimeStampedData>;
+  std::unique_ptr<sink_t> outputQueues_;
+  std::chrono::milliseconds queueTimeout_;
+
+  // Random Generatior
+  std::vector<TimeStampedData> GetTimestamp();
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> rdm_signaltype = std::uniform_int_distribution<int>    (0, 2);
+};
+} // namespace trigger
+
+ERS_DECLARE_ISSUE_BASE(trigger,
+                       NoOutputQueuesAvailableWarning,
+                       appfwk::GeneralDAQModuleIssue,
+                       "No output queues were available, so the generated list of integers will be dropped. Has initialization been successfully completed?",
+                       ((std::string)name),
+                       ERS_EMPTY)
+
+} // namespace dunedaq
+
+#endif // TRIGGER_TEST_PLUGINS_FAKETIMESTAMPDATAGENERATOR_HPP_
+
+// Local Variables:
+// c-basic-offset: 2
+// End:
