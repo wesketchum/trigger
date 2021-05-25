dc8cf3699d0e7bc43ef0f1aacbd6a2dbf69fdc5f
Florian Grötschla <florian.till.grotschla@cern.ch>
2021-05-25 16:46:40 +0200

    Make GeoID configurable

--- plugins/ModuleLevelTrigger.cpp
+++ plugins/ModuleLevelTrigger.cpp
@@ -87,9 +87,8 @@ ModuleLevelTrigger::do_configure(const nlohmann::json& confobj)
 
   m_links.clear();
   for (auto const& link : params.links) {
-    // For the future: Set APA properly
     m_links.push_back(
-      dfmessages::GeoID{ dataformats::GeoID::SystemType::kTPC, 0, static_cast<uint32_t>(link) }); // NOLINT
+        dfmessages::GeoID{ dataformats::GeoID::string_to_system_type(link.system), link.region, link.element });
   }
 
   m_configured_flag.store(true);
