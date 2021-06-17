455b7529656acf5f66d37685b01b6cfc9f1f8724
Benjamin Land <benland100@gmail.com>
2021-06-17 17:07:15 -0400

    Add prescale maker plugins and faketp_chain app.
    See the README.md in python/trigger/faketp_chain

---
+++ plugins/TriggerActivityMakerPrescalePlugin.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file TriggerActivityMakerSupernova.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+ 
+#include "trigger/AlgorithmPlugins.hpp"
+#include "triggeralgs/Prescale/TriggerActivityMakerPrescale.hpp"
+
+DEFINE_DUNE_TA_MAKER(triggeralgs::TriggerActivityMakerPrescale)
