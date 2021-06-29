0352739aca4b23ddf5d02e77f06e67c7f4ad359f
Brett Viren <brett.viren@gmail.com>
2021-06-29 11:13:08 -0400

    Add minimal TxSet unit test

--- unittest/TxSet_test.cxx
+++ unittest/TxSet_test.cxx
@@ -41,7 +41,7 @@ BOOST_TEST_GLOBAL_FIXTURE(TxSetTestFixture);
 BOOST_AUTO_TEST_CASE(Initialization)
 {
     trigger::TPSet tpset;
-    BOOST_CHECK_EQUAL(tpset.origin.syste_type == dataformats::GeoID::SystemType::kDatatSelection);
+    BOOST_CHECK_EQUAL(tpset.origin.system_type, dataformats::GeoID::SystemType::kDataSelection);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
