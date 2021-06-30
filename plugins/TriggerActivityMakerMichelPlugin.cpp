598e6204570a222e128cffccb3f71582e65e671d
nkassein <nak2150@columbia.edu>
2021-06-30 10:34:23 -0400

    Added Michel Trigger Plugins

---
+++ plugins/TriggerActivityMakerMichelPlugin.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file TriggerActivityMakerMichel.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+ 
+#include "trigger/AlgorithmPlugins.hpp"
+#include "triggeralgs/Michel/TriggerActivityMakerMichel.hpp"
+
+DEFINE_DUNE_TA_MAKER(triggeralgs::TriggerActivityMakerMichel)
---
+++ plugins/TriggerCandidateMakerMichelPlugin.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file TriggerCandidateMakerMichel.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "trigger/AlgorithmPlugins.hpp"
+#include "triggeralgs/Michel/TriggerCandidateMakerMichel.hpp"
+
+DEFINE_DUNE_TC_MAKER(triggeralgs::TriggerCandidateMakerMichel)
