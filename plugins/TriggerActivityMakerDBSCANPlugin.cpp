688a059921a0e44ac001a652428abe03a852d64b
Philip Rodrigues <rodriges@fnal.gov>
2021-08-20 16:21:38 +0200

    Add plugin for TriggerActivityMakerDBSCAN

---
+++ plugins/TriggerActivityMakerDBSCANPlugin.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file TriggerActivityMakerDBSCANPlugin.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "trigger/AlgorithmPlugins.hpp"
+#include "triggeralgs/dbscan/TriggerActivityMakerDBSCAN.hpp"
+
+DEFINE_DUNE_TA_MAKER(triggeralgs::TriggerActivityMakerDBSCAN)
